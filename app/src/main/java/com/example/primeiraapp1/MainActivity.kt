e8022376e0b4227dee227c2c1fff789a989f340b
Vitu-VI <palmeiras.victor27@gmail.com>
2025-05-01 22:50:51 -0300

    teste 1

---
+++ app/src/main/java/com/example/primeiraapp1/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.primeiraapp1
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.primeiraapp1.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+
+        setContentView(binding.root)
+
+
+            binding.buttonOla.setOnClickListener {
+                val nome: String = binding.editNome.text.toString()
+                binding.textResultado.text = "Olá ${nome}"
+            }
+        }
+    }
